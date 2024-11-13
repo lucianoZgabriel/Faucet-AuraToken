9ce50c48f0d494b0a18d3c8e8f905c6005a19ef2
lucianoZgabriel <lucianozanin@gmail.com>
2024-11-13 14:19:52 -0400

    chore: integration back/front

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,14 @@ dotenv.config();
 
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { mintAndTransfer } from "./Web3Provider";
 
 const PORT: number = parseInt(`${process.env.PORT || 3001}`);
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(cors());
 
 app.post(
   "/mint/:wallet",
@@ -16,7 +18,8 @@ app.post(
     try {
       const tx = await mintAndTransfer(req.params.wallet);
       res.status(200).json(tx);
-    } catch (error) {
+    } catch (error: any) {
+      console.log(error.cause.message);
       res.status(500).json(error);
     }
   }
