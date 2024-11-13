0885d53e23bf95cd34916098e5e317d2f626baae
lucianoZgabriel <lucianozanin@gmail.com>
2024-11-13 10:24:43 -0400

    feat: add webprovider

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,24 @@ dotenv.config();
 
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
+import { mintAndTransfer } from "./Web3Provider";
 
 const PORT: number = parseInt(`${process.env.PORT || 3001}`);
 const app = express();
 
 app.use(morgan("tiny"));
 
-app.post("/mint/:wallet", (req: Request, res: Response, next: NextFunction) => {
-  res.json(true);
-});
+app.post(
+  "/mint/:wallet",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const tx = await mintAndTransfer(req.params.wallet);
+      res.status(200).json(tx);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
